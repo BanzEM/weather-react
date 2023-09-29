7ba0c4a5773f853716285607544c43db86499f0e
BanzEM <school.honours@gmail.com>
2023-09-29 12:13:12 +0200

    created app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          coded by Bandisile Mazomba
+          <a
+            className="App-link"
+            href="./"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
